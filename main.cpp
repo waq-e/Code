5c158dadfc014b45fc9efbd7cd6dbdcfcf83d715
waq-e <whomomlikes@hotmail.com>
2022-03-10 11:00:26 -0600

    Fig. 3.1 & 3.2

--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@
 #include <iostream>
 #include <string>
 #include "Account.h"
+
 using namespace std;
+
 int main() {
 	Account myAccount; // create Account object myAccount
-// show that the initial value of myAccount's name is the empty string
+
+	// show that the initial value of myAccount's name is the empty string
 	cout << "Initial account name is: " << myAccount.getName();
-// prompt for and read name
+
+	// prompt for and read name
 	cout << "\nPlease enter the account name: ";
 	string theName;
 	getline(cin, theName); // read a line of text
 	myAccount.setName(theName); // put theName in myAccount
-// display the name stored in object myAccount
+
+	// display the name stored in object myAccount
 	cout << "Name in object myAccount is: "
 	     << myAccount.getName() << endl;
 }
