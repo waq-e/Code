fcd607827283804468bfb3ac5f89a4445a9e421f
waq-e <whomomlikes@hotmail.com>
2022-03-10 11:46:14 -0600

    Fig. 3.5

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-// Fig. 3.1: AccountTest.cpp
-// Creating and manipulating an Account object.
+// Fig. 3.5: AccountTest.cpp
+// Using the Account constructor to initialize the name data
+// member at the time each Account object is created.
 #include <iostream>
-#include <string>
 #include "Account.h"
 
 using namespace std;
 
 int main() {
-	Account myAccount; // create Account object myAccount
+	// create two Account objects
+	Account account1{"Jane Green"};
+	Account account2{"John Blue"};
 
-	// show that the initial value of myAccount's name is the empty string
-	cout << "Initial account name is: " << myAccount.getName();
-
-	// prompt for and read name
-	cout << "\nPlease enter the account name: ";
-	string theName;
-	getline(cin, theName); // read a line of text
-	myAccount.setName(theName); // put theName in myAccount
-
-	// display the name stored in object myAccount
-	cout << "Name in object myAccount is: "
-	     << myAccount.getName() << endl;
+	// display initial value of name for each Account
+	cout << "account1 name is: " << account1.getName() << endl;
+	cout << "account2 name is: " << account2.getName() << endl;
 }
