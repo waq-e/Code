221a685bb8745b75d67a2acded340bd3e7bf2f73
waq-e <whomomlikes@hotmail.com>
2022-03-10 12:01:07 -0600

    Fig. 3.8 & 3.9

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
-// Fig. 3.5: AccountTest.cpp
-// Using the Account constructor to initialize the name data
-// member at the time each Account object is created.
+// Fig. 3.9: AccountTest.cpp
+// Displaying and updating Account balances.
 #include <iostream>
 #include "Account.h"
 
 using namespace std;
 
-int main() {
-	// create two Account objects
-	Account account1{"Jane Green"};
-	Account account2{"John Blue"};
+int main()
+{
+	Account account1{"Jane Green", 50};
+	Account account2{"John Blue", -7};
 
-	// display initial value of name for each Account
-	cout << "account1 name is: " << account1.getName() << endl;
-	cout << "account2 name is: " << account2.getName() << endl;
+	// display initial balance of each object
+	cout << "account1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance();
+
+	cout << "\n\nEnter deposit amount for account1: "; // prompt
+	int depositAmount;
+	cin >> depositAmount; // obtain user input
+	cout << "adding " << depositAmount << " to account1 balance";
+	account1.deposit(depositAmount); // add to account1's balance
+
+	// display balances
+	cout << "\n\naccount1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance();
+
+	cout << "\n\nEnter deposit amount for account2: "; // prompt
+	cin >> depositAmount; // obtain user input
+	cout << "adding " << depositAmount << " to account2 balance";
+	account2.deposit(depositAmount); // add to account2 balance
+
+	// display balances
+	cout << "\n\naccount1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance() << endl;
 }
