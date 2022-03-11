ce401eae423b965c703c327eb83548c2ac17550d
waq-e <whomomlikes@hotmail.com>
2022-03-11 02:57:09 -0600

    Fig. 4.10

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
-// Fig. 4.7: StudentTest.cpp
-// Create and test Student objects.
+// Fig. 4.10: ClassAverage.cpp
+// Solving the class-average problem using counter-controlled iteration.
 #include <iostream>
-#include "Student.h"
 using namespace std;
 
 int main() {
-	Student account1{"Jane Green", 93};
-	Student account2{"John Blue", 72};
+	// initialization phase
+	int total{0}; // initialize sum of grades entered by the user
+	unsigned int gradeCounter{1}; // initialize grade # to be entered next
 
-	cout << account1.getName() << "'s letter grade equivalent of "
-	     << account1.getAverage() << " is: "
-	     << account1.getLetterGrade() << "\n";
-	cout << account2.getName() << "'s letter grade equivalent of "
-	     << account2.getAverage() << " is: "
-	     << account2.getLetterGrade() << endl;
+	// processing phase uses counter-controlled iteration
+	while (gradeCounter <= 10) { // loop 10 times
+		cout << "Enter grade: "; // prompt
+		int grade;
+		cin >> grade; // input next grade
+		total = total + grade; // add grade to total
+		gradeCounter = gradeCounter + 1; // increment counter by 1
+	}
+
+	// termination phase
+	int average{total / 10}; // int division yields int result
+
+	// display total and average of grades
+	cout << "\nTotal of all 10 grades is " << total;
+	cout << "\nClass average is " << average << endl;
 }
