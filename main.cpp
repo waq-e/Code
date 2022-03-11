401ee96421b47dc4dface7263de3f02de404ed80
waq-e <whomomlikes@hotmail.com>
2022-03-11 05:11:16 -0600

    Fig. 4.17

--- main.cpp
+++ main.cpp
@@ -1,38 +1,20 @@
-// Fig. 4.14: Analysis.cpp
-// Analysis of examination results using nested control statements.
+// Fig. 4.17: Increment.cpp
+// Prefix increment and postfix increment operators.
 #include <iostream>
 using namespace std;
 
 int main() {
-	// initializing variables in declarations
-	unsigned int passes{0};
-	unsigned int failures{0};
-	unsigned int studentCounter{1};
+	// demonstrate postfix increment operator
+	unsigned int c{5}; // initializes c with the value 5
+	cout << "c before postincrement: " << c << endl; // prints 5
+	cout << "  	 postincrementing c: " << c++ << endl; // prints 5
+	cout << " c after postincrement: " << c << endl; // prints 6
 
-	// process 10 students using counter-controlled loop
-	while (studentCounter <= 10) {
-		// prompt user for input and obtain value from user
-		cout << "Enter result (1 = pass, 2 = fail): ";
-		int result;
-		cin >> result;
+	cout << endl; // skip a line
 
-		// if...else is nested in the while statement
-		if (result == 1) {
-			passes = passes + 1;
-		}
-		else {
-			failures = failures + 1;
-		}
-
-		// increment studentCounter so loop eventually terminates
-		studentCounter = studentCounter + 1;
-	}
-
-	// termination phase; prepare and display results
-	cout << "Passed: " << passes << "\nFailed: " << failures << endl;
-
-	// determine whether more than 8 students passed
-	if (passes > 8) {
-		cout << "Bonus to instructor!" << endl;
-	}
+	// demonstrate prefix increment operator
+	c = 5; // assigns 5 to c
+	cout << " c before preincrement: " << c << endl; // prints 5
+	cout << "	  preincrementing c: " << ++c << endl; // prints 6
+	cout << "  c after preincrement: " << c << endl; // prints 6
 }
