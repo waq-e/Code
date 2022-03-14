22db24b3c7cd18ff8cd4e323bcf484423517af7d
waq-e <whomomlikes@hotmail.com>
2022-03-14 07:47:46 -0500

    Fig. 5.7 & 5.8

--- main.cpp
+++ main.cpp
@@ -1,29 +1,51 @@
-// Fig. 5.6: Interest.cpp
-// Compound-interest calculations with for.
+// Fig. 5.7: Interest.cpp
+// Compound-interest calculations with class DollarAmount and integers.
 #include <iostream>
 #include <iomanip>
-#include <cmath> // for pow function
+#include <string>
+#include "DollarAmount.h"
 using namespace std;
 
 int main() {
-	// set floating-point number format
-	cout << fixed << setprecision(2);  // fixed & setprecision are sticky.
+	DollarAmount d1{12345}; // $123.45
+	DollarAmount d2{1576}; // $15.76
 
-	double principal{1000.00}; // initial amount before interest
-	double rate{0.05}; // interest rate
+	cout << "After adding d2 (" << d2.toString() << ") into d1 ("
+	     << d1.toString() << "), d1 = ";
+	d1.add(d2); // modifies object d1
+	cout << d1.toString() << "\n";
 
-	cout << "Initial principal: " << principal << endl;
-	cout << "    Interest rate: " << rate << endl;
+	cout << "After subtracting d2 (" << d2.toString() << ") from d1 ("
+	     << d1.toString() << "), d1 = ";
+	d1.subtract(d2); // modifies object d1
+	cout << d1.toString() << "\n";
+
+	cout << "After subtracting d1 (" << d1.toString() << ") from d2 ("
+	     << d2.toString() << "), d2 = ";
+	d2.subtract(d1); // modifies object d2
+	cout << d2.toString() << "\n\n";
+
+	cout << "Enter integer interest rate and divisor. For example:\n"
+		    << "for	2%, enter:	      2 100\n"
+			<< "for	2.3%, enter:	 23 1000\n"
+			<< "for 2.37%, enter:   237 10000\n"
+			<< "for 2.375%, enter: 2375 100000\n> ";
+	int rate; // whole-number interest rate
+	int divisor; // divisor for rate
+	cin >> rate >> divisor;
+
+	DollarAmount balance{100000}; // initial principal amount in pennies
+	cout << "\nInitial balance: " << balance.toString() << endl;
 
 	// display headers
-	cout << "\nYear" << setw(20) << "Amount on deposit" << endl;  // setw non sticky.
+	cout << "\nYear" << setw(20) << "Amount on deposit" << endl;
 
 	// calculate amount on deposit for each of ten years
 	for (unsigned int year{1}; year <= 10; year++) {
-		// calculate amount on deposit at the end of the specified year
-		double amount = principal * pow(1.0 + rate, year);
+		// increase balance by rate % (i.e., rate / divisor)
+		balance.addInterest(rate, divisor);
 
 		// display the year and the amount
-		cout << setw(4) << year << setw(20) << amount << endl;
+		cout << setw(4) << year << setw(20) << balance.toString() << endl;
 	}
 }
