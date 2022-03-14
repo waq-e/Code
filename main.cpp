8aa0dd366cba36e2a72e7f4a3dbd277d6e01b89f
waq-e <whomomlikes@hotmail.com>
2022-03-14 07:18:07 -0500

    Fig. 5.1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
-// Fig. 4.17: Increment.cpp
-// Prefix increment and postfix increment operators.
+// Fig. 5.1: WhileCounter.cpp
+// Counter-controlled iteration with the while iteration statement.
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	// demonstrate postfix increment operator
-	unsigned int c{5}; // initializes c with the value 5
-	cout << "c before postincrement: " << c << endl; // prints 5
-	cout << "  	 postincrementing c: " << c++ << endl; // prints 5
-	cout << " c after postincrement: " << c << endl; // prints 6
+	unsigned int counter{1}; // declare and initialize control variable
 
-	cout << endl; // skip a line
-
-	// demonstrate prefix increment operator
-	c = 5; // assigns 5 to c
-	cout << " c before preincrement: " << c << endl; // prints 5
-	cout << "	  preincrementing c: " << ++c << endl; // prints 6
-	cout << "  c after preincrement: " << c << endl; // prints 6
+	while (counter <= 10) { // loop-continuation condition
+		cout << counter << " ";
+		++counter; // increment control variable
+	}
+	cout << endl;
 }
