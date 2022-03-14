5fce76cb70ae66c3460d9e51fff28f7ca2a4b97d
waq-e <whomomlikes@hotmail.com>
2022-03-14 07:28:37 -0500

    Fig. 5.5

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-// Fig. 5.2: ForCounter.cpp
-// Counter-controlled iteration with the for iteration statement.
+// Fig. 5.5: Sum.cpp
+// Summing integers with the for statement.
 #include <iostream>
 using namespace std;
 
 int main() {
-// for statement header includes initialization,
-// loop-continuation condition and increment
-	for (unsigned int counter{1}; counter <= 10; ++counter) {
-		cout << counter << " ";
+	unsigned int total{0};
+
+	// total even integers from 2 through 20
+	for (unsigned int number{2}; number <= 20; number += 2) {
+		total += number;
 	}
 
-	cout << endl;
+	cout << "Sum is " << total << endl;
 }
